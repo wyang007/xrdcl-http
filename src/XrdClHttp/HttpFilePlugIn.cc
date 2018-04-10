143dd37bd0ca6aa7ae5edb0b96dd5aea50338658
Radu Popescu <mail@radupopescu.net>
2018-04-10 16:06:32 +0200

    Fixing indentation

--- src/XrdClHttp/HttpFilePlugIn.cc
+++ src/XrdClHttp/HttpFilePlugIn.cc
@@ -30,84 +30,84 @@ HttpFilePlugIn::HttpFilePlugIn()
 HttpFilePlugIn::~HttpFilePlugIn() {}
 
 XRootDStatus HttpFilePlugIn::Open( const std::string &url,
-                   OpenFlags::Flags   flags,
-                   Access::Mode       mode,
-                   ResponseHandler   *handler,
-                   uint16_t           timeout )
+                                   OpenFlags::Flags   flags,
+                                   Access::Mode       mode,
+                                   ResponseHandler   *handler,
+                                   uint16_t           timeout )
 {
   (void)url; (void)flags; (void)mode; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Close( ResponseHandler *handler,
-                    uint16_t         timeout )
+                                    uint16_t         timeout )
 {
   (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Stat( bool             force,
-                   ResponseHandler *handler,
-                   uint16_t         timeout )
+                                   ResponseHandler *handler,
+                                   uint16_t         timeout )
 {
   (void)force; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Read( uint64_t         offset,
-                   uint32_t         size,
-                   void            *buffer,
-                   ResponseHandler *handler,
-                   uint16_t         timeout )
+                                   uint32_t         size,
+                                   void            *buffer,
+                                   ResponseHandler *handler,
+                                   uint16_t         timeout )
 {
   (void)offset; (void)size; (void)buffer; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Write( uint64_t         offset,
-                    uint32_t         size,
-                    const void      *buffer,
-                    ResponseHandler *handler,
-                    uint16_t         timeout )
+                                    uint32_t         size,
+                                    const void      *buffer,
+                                    ResponseHandler *handler,
+                                    uint16_t         timeout )
 {
   (void)offset; (void)size; (void)buffer; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Sync( ResponseHandler *handler,
-                   uint16_t         timeout )
+                                   uint16_t         timeout )
 {
   (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Truncate( uint64_t         size,
-                       ResponseHandler *handler,
-                       uint16_t         timeout )
+                                       ResponseHandler *handler,
+                                       uint16_t         timeout )
 {
   (void)size; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::VectorRead( const ChunkList &chunks,
-                         void            *buffer,
-                         ResponseHandler *handler,
-                         uint16_t         timeout )
+                                         void            *buffer,
+                                         ResponseHandler *handler,
+                                         uint16_t         timeout )
 {
   (void)chunks; (void)buffer; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Fcntl( const Buffer    &arg,
-                    ResponseHandler *handler,
-                    uint16_t         timeout )
+                                    ResponseHandler *handler,
+                                    uint16_t         timeout )
 {
   (void)arg; (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
 }
 
 XRootDStatus HttpFilePlugIn::Visa( ResponseHandler *handler,
-                   uint16_t         timeout )
+                                   uint16_t         timeout )
 {
   (void)handler; (void)timeout;
   return XRootDStatus( stError, errNotImplemented );
@@ -119,14 +119,14 @@ bool HttpFilePlugIn::IsOpen() const
 }
 
 bool HttpFilePlugIn::SetProperty( const std::string &name,
-                  const std::string &value )
+                                  const std::string &value )
 {
   (void)name; (void)value;
   return false;
 }
 
 bool HttpFilePlugIn::GetProperty( const std::string &name,
-                  std::string &value ) const
+                                  std::string &value ) const
 {
   (void)name; (void)value;
   return false;
