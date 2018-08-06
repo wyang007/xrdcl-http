64e50f92ef86fd92e0d926b7a875f94b04a6f77c
Radu Popescu <mail@radupopescu.net>
2018-08-06 11:16:06 +0200

    Add implementation of unsupported methods in HttpFileSystemPlugIn

--- src/XrdClHttp/HttpFileSystemPlugIn.cc
+++ src/XrdClHttp/HttpFileSystemPlugIn.cc
@@ -26,7 +26,35 @@ HttpFileSystemPlugIn::HttpFileSystemPlugIn(const std::string &url)
                  url_.GetURL().c_str());
 }
 
-XRootDStatus HttpFileSystemPlugIn::Stat(const std::string& path,
+XRootDStatus HttpFileSystemPlugIn::Truncate(const std::string &path,
+                                            uint64_t size,
+                                            ResponseHandler *handler,
+                                            uint16_t timeout) {
+  (void)path;
+  (void)size;
+  (void)handler;
+  (void)timeout;
+
+  logger_->Error(kLogXrdClHttp, "Truncate not supported with HTTP");
+
+  return XRootDStatus(stError, errNotSupported);
+}
+
+XRootDStatus HttpFileSystemPlugIn::ChMod(const std::string &path,
+                                         Access::Mode mode,
+                                         ResponseHandler *handler,
+                                         uint16_t timeout) {
+  (void)path;
+  (void)mode;
+  (void)handler;
+  (void)timeout;
+
+  logger_->Error(kLogXrdClHttp, "ChMod not supported with HTTP");
+
+  return XRootDStatus(stError, errNotSupported);
+}
+
+XRootDStatus HttpFileSystemPlugIn::Stat(const std::string &path,
                                         ResponseHandler *handler,
                                         uint16_t timeout) {
   logger_->Debug(kLogXrdClHttp,
@@ -37,7 +65,8 @@ XRootDStatus HttpFileSystemPlugIn::Stat(const std::string& path,
   Davix::DavPosix davix_client(&ctx);
 
   auto stat_info = new StatInfo();
-  auto status = Posix::Stat(davix_client, url_.GetLocation(), timeout, stat_info);
+  auto status =
+      Posix::Stat(davix_client, url_.GetLocation(), timeout, stat_info);
 
   if (status.IsError()) {
     logger_->Error(kLogXrdClHttp, "Stat failed: %s", status.ToStr().c_str());
@@ -52,15 +81,13 @@ XRootDStatus HttpFileSystemPlugIn::Stat(const std::string& path,
 }
 
 bool HttpFileSystemPlugIn::SetProperty(const std::string &name,
-                                       const std::string &value)
-{
+                                       const std::string &value) {
   properties_[name] = value;
   return true;
 }
 
 bool HttpFileSystemPlugIn::GetProperty(const std::string &name,
-                                       std::string &value) const
-{
+                                       std::string &value) const {
   const auto p = properties_.find(name);
   if (p == std::end(properties_)) {
     return false;
--- src/XrdClHttp/HttpFileSystemPlugIn.hh
+++ src/XrdClHttp/HttpFileSystemPlugIn.hh
@@ -19,6 +19,14 @@ class HttpFileSystemPlugIn : public FileSystemPlugIn {
   HttpFileSystemPlugIn(const std::string &url);
   virtual ~HttpFileSystemPlugIn() noexcept = default;
 
+  virtual XRootDStatus Truncate(const std::string &path, uint64_t size,
+                                ResponseHandler *handler,
+                                uint16_t timeout) override;
+
+  virtual XRootDStatus ChMod(const std::string &path, Access::Mode mode,
+                             ResponseHandler *handler,
+                             uint16_t timeout) override;
+
   virtual XRootDStatus Stat(const std::string &path, ResponseHandler *handler,
                             uint16_t timeout) override;
 
