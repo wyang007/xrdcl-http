705c5441e7a140a1a97dd2035bc441b0384a183c
Michal Simon <michal.simon@cern.ch>
2019-04-17 17:03:38 +0200

    Update FindXrdCl cmake module so it can be used if built as a submodule of core XRootD project.

--- cmake/FindXrdCl.cmake
+++ cmake/FindXrdCl.cmake
@@ -1,23 +1,29 @@
-find_path(
-    XrdCl_INCLUDE_DIRS
-    NAMES xrootd/XrdCl/XrdClPlugInInterface.hh
-    HINTS ${XrdCl_INCLUDE_DIRS}
-)
+if( XRDCLHTTP_SUBMODULE )
+  set( XrdCl_INCLUDE_DIRS ${CMAKE_SOURCE_DIR/src} )
+  set( XrdCl_LIBRARIES    ${CMAKE_BINARY_DIR/src} )
+  set( XrdCl_FOUND        TRUE )
+else()
+  find_path(
+      XrdCl_INCLUDE_DIRS
+      NAMES xrootd/XrdCl/XrdClPlugInInterface.hh
+      HINTS ${XrdCl_INCLUDE_DIRS}
+  )
 
-find_library(
-    XrdCl_LIBRARIES
-    NAMES XrdCl
-    HINTS ${XrdCl_LIBRARY_DIRS}
-)
+  find_library(
+      XrdCl_LIBRARIES
+      NAMES XrdCl
+      HINTS ${XrdCl_LIBRARY_DIRS}
+  )
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    XrdCl
-    DEFAULT_MSG
-    XrdCl_LIBRARIES
-    XrdCl_INCLUDE_DIRS
-)
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+      XrdCl
+      DEFAULT_MSG
+      XrdCl_LIBRARIES
+      XrdCl_INCLUDE_DIRS
+  )
+endif()
 
 if(XrdCl_FOUND)
-    mark_as_advanced(XrdCl_LIBRARIES XrdCl_INCLUDE_DIRS)
+  mark_as_advanced(XrdCl_LIBRARIES XrdCl_INCLUDE_DIRS)
 endif()
